fa782b2d81ef8ffa2803e211fd3296a619e575be
Zaïd.ckh <zaidchikh.910@gmail.com>
2025-05-17 08:27:24 +0200

    a rock paper scissors program

---
+++ script.js
@@ -0,0 +1,91 @@
+let number
+
+let computerChoice
+let humanChoice
+
+let rock = "rock"
+let paper = "paper"
+let scissors = "scissors"
+
+
+function getComputerChoice(number) {
+    number = Math.floor(Math.random() * 3) + 1;
+
+    if (number === 1)  {
+        computerChoice = rock
+    } else if (number === 2) {
+        computerChoice = paper
+    } else {
+        computerChoice = scissors
+    } 
+
+    return computerChoice
+}
+
+function getHumanChoice() {
+    humanChoice = prompt("rock, paper or scissors ?")
+
+    humanChoice = humanChoice.toLowerCase()
+
+    if (humanChoice === "rock") {
+        return rock
+    } else if (humanChoice === "paper") {
+        return paper
+    } else if (humanChoice === "scissors") {
+        return scissors
+    } else {
+        humanChoice = prompt("rock, paper or scissors ?")
+    }
+
+    return humanChoice
+}
+
+let humanScore = 0
+let computerScore = 0
+
+function playRound(humanChoice, computerChoice) {
+    const humanSelection = getHumanChoice()
+    const computerSelection = getComputerChoice()
+    
+    if (humanSelection == "rock" && computerSelection == "scissors") {
+        console.log("You won, rock beat scissors !")
+        humanScore++
+    }   else if (humanSelection == "rock" && computerSelection == "paper") {
+        console.log("You loose, paper beat rock !")
+        computerScore++
+    }   else if (humanSelection == "rock" && computerSelection == "rock") {
+        console.log("Draw, rock and rock")
+    }
+
+    if (humanSelection == "paper" && computerSelection == "rock") {
+        console.log("You won, paper beat rock !")
+        humanScore++
+    }   else if (humanSelection == "paper" && computerSelection == "scissors") {
+        console.log("You loose, scissors beat paper !")
+        computerScore++
+    }   else if (humanSelection == "paper" && computerSelection == "paper") {
+        console.log("Draw, paper and paper")
+    }
+
+    if (humanSelection == "scissors" && computerSelection == "paper") {
+        console.log("You won, scissors beat paper !")
+        humanScore++
+    }   else if (humanSelection == "scissors" && computerSelection == "rock") {
+        console.log("You loose, rock beat scissors !")
+        computerScore++
+    }   else if (humanSelection == "scissors" && computerSelection == "scissors") {
+        console.log("Draw, scissors and scissors")
+    }
+
+    return humanScore + " " + computerScore
+}
+
+function playGame() {
+
+    for (let i = 0; i < 5; i++) {
+        playRound()
+    }
+    return humanScore + " " + computerScore
+}
+
+console.log(playGame())
